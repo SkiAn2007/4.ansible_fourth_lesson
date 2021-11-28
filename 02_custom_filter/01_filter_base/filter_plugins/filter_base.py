c241d057b86680d4c586d624e016ac4249b99232
ansible <ansible@ansible-control.localdomain>
2021-11-29 00:28:01 +0300

    HW final part

--- 02_custom_filter/01_filter_base/filter_plugins/filter_base.py
+++ 02_custom_filter/01_filter_base/filter_plugins/filter_base.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
-def filter_function(variable):
+from ansible.errors import (
+    AnsibleFilterError,
+    AnsibleFilterTypeError
+)
+def concat_filter(var_a, var_b):
     '''
-        function doc
+        concat 2 strings
     '''
-    return variable
+    if not isinstance(var_a, str):
+        raise AnsibleFilterTypeError("String type is expected, "
+                                     "got type %s instead" % type(var_a))
+
+    if not isinstance(var_b, str):
+        raise AnsibleFilterTypeError("String type is expected, "
+                                     "got type %s instead" % type(var_b))
+
+    return str(var_a + var_b)
+
+def split_filter(input_str, input_separator=' '):
+    '''
+        split string into list of strings with particular separator
+    '''
+    if not isinstance(input_str, str):
+        raise AnsibleFilterTypeError("String type is expected, "
+                                     "got type %s instead" % type(input_str))
+    return str(input_str).split(sep=input_separator)
 
 
 class FilterModule(object):
     def filters(self):
         return {
-            'filter_function': filter_function
+            'concat_filter': concat_filter,
+            'split_filter': split_filter
         }
